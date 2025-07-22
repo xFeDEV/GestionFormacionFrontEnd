0299e4a6063734acd1bfb354f66b1ee967f28f76
Edison Betancur <edfbetancur@gmail.com>
2025-07-22 14:26:52 -0500

    fix: appsidebar

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -19,7 +19,7 @@ import {
   UserCircleIcon,
 } from "../icons";
 import { useSidebar } from "../context/SidebarContext";
-import SidebarWidget from "./SidebarWidget";
+
 
 type NavItem = {
   name: string;
