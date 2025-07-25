33b3e2894aab3964758a9d586f4d9ddf036872e8
Edison Betancur <edison_fbetancur@soy.sena.edu.co>
2025-07-24 19:35:05 -0500

    fix: Modificando estilos de Modality.tsx

--- src/components/groupGraph/Modality.tsx
+++ src/components/groupGraph/Modality.tsx
@@ -44,7 +44,7 @@ export default function ModalityChart() {
     },
   };
 
-  const series = [44, 55, 22];
+  const series = [44, 57, 22];
  
   return (
     <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
